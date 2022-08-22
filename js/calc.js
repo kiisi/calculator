705c05de4a705f156e7237f2624970cd37d1c661
kiisi <kiisifelix06@gmail.com>
2022-08-22 17:24:44 +0100

    first commit

---
+++ js/calc.js
@@ -0,0 +1,81 @@
+function convertToInt(i){
+    let num = parseInt(i);
+    return num
+}
+
+
+
+function calculator(number){
+    let operations = ['*', '-', '+', '/']
+    let subOperations = ['*','/']
+    let opera = ['+','-']
+
+
+    let sepArray = []
+
+    let initValue = 0;
+    let sign = "+"
+
+    for(let i = 0; i < number.length; i++){
+        if(convertToInt(number[i])){
+            sepArray.push(convertToInt(number[i]))
+        }else{
+            sepArray.push(number[i])
+        }
+    }
+    let filterSignArray = []
+
+    for(let i = 0; i < sepArray.length; i++){
+        if(typeof(sepArray[i]) === 'number'){
+            if(opera.includes(sepArray[i - 1])){
+                if(sepArray[i - 1] === '-'){
+                    let num = -1 * sepArray[i]
+                    filterSignArray.push(num)
+                }
+                if(sepArray[i - 1] === '+'){
+                    filterSignArray.push(sepArray[i])
+                }   
+            }else{
+                filterSignArray.push(sepArray[i])
+            }
+        }
+        if(subOperations.includes(sepArray[i])){
+            filterSignArray.push(sepArray[i])
+        }
+    }
+
+    let output_calc = outputCalc(filterSignArray)
+    return output_calc
+}
+
+function outputCalc(arr){
+    let initValue = 0, prev, cur;
+
+    for (let i = 0; i < arr.length; i++){
+        if(typeof(arr[i]) === 'number'){
+            if(arr[i] < 0){
+                let num = -1 * arr[i];
+                initValue -= num;
+            }
+            else{
+                initValue += arr[i]
+            }
+        }
+        else{
+            if(arr[i] === '*'){
+                initValue *= arr[i + 1];
+                i +=1;
+            }
+            else{
+                initValue /= arr[i + 1];
+                i +=1;
+            }
+
+        }
+    }
+
+    return initValue;
+}
+
+
+export default calculator
---
+++ js/main.js
@@ -0,0 +1,61 @@
+import calculator from './calc.js';
+
+
+let board = document.querySelector('.board')
+let button = document.querySelector('.equal')
+
+button.addEventListener('click', ()=>{
+    let input = board.value
+    console.log(calc(input));
+});
+
+
+
+function calc(input){
+    let array = [];
+    let operations = ['*', '-', '+', '/']
+    let subOperations = ['*','/']
+
+    for (let i = 0; i < input.length; i++){
+        if((i === 0 && subOperations.includes(input.at(i)))){
+            return "Syntax Error 1"
+        }
+        else if((i > 0 && subOperations.includes(input.at(i)) && subOperations.includes(input.at(i + 1)) && i < input.length)){
+            return "Syntax Error 2"
+        }
+        else if((i === input.length - 1) && operations.includes(input.at(i)) 
+        ){
+            return "Syntax Error 3"
+        }
+        else if((operations.includes(input.at(i))) && subOperations.includes(input.at(i + 1))){
+            return "Syntax Error 4"
+        }
+        array.push(input[i])
+    }
+    let mainArray = numbersArray(array)
+    return mainArray;
+}
+
+function numbersArray(arr){
+    let joinNum = arr.join('')
+    let numArray = []
+    let prev = 0;
+    for(let i = 0; i < joinNum.length; i++){
+        if(joinNum[i] == '*' || joinNum[i] == '+' || joinNum[i] == '-' || joinNum[i] == '/'){
+            numArray.push(joinNum.slice(prev, i))
+            numArray.push(joinNum[i])
+            prev = i + 1
+        }
+        if(i === joinNum.length - 1){
+            numArray.push(joinNum.slice(prev,joinNum.length))
+        }
+    }
+
+    let refNumArray = refineNumArray(numArray)
+    return refNumArray
+}
+
+function refineNumArray(arr){
+    let refinedArray = arr.filter(e=> e !== '')
+    return calculator(refinedArray)
+}
