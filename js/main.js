8cb1b130399785b2848a916031fb4d79085534c9
kiisi <kiisifelix06@gmail.com>
2022-08-23 13:38:28 +0100

    Done

--- js/main.js
+++ js/main.js
@@ -111,9 +111,11 @@ const useDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
 if(useDarkTheme.matches){
     darkthemeIcon.textContent = 'dark_mode'
     darktheme.classList.add("sidenav_effect")
+    document.documentElement.classList.add("calc-darkmode")
 }else{
     darktheme.classList.remove("sidenav_effect");
     darkthemeIcon.textContent = 'light_mode';
+    document.documentElement.classList.remove("calc-darkmode")
 }
 
 function toggleDarkMode(state){
